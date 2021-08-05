a3492940e797be97b420064ebddc488e07b3c3e2
Aaron Surty <aarons@actionsoftware.com>
2021-08-05 15:23:36 -0400

    adding express server for loading board data from web

---
+++ boards/server.js
@@ -0,0 +1,22 @@
+const handler = require('serve-handler');
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((request, response) => {
+  // You pass two more arguments for config and middleware
+  // More details here: https://github.com/vercel/serve-handler#options
+  var pathname = url.parse(request.url).pathname;
+  console.log(pathname);
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Request-Method', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  return handler(request, response);
+
+})
+
+port = 5054
+
+server.listen(port, () => {
+  console.log('Running at http://localhost:'+port);
+});
